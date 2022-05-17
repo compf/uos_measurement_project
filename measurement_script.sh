0e8dced6c2fd9ed2b529a409fd672a02aaed7c41
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-05-17 13:46:59 +0200

    small fix

--- measurement_script.sh
+++ measurement_script.sh
@@ -1,6 +1,6 @@
 #env/bash
 ts=$(date +%s)
 cd /home/pi/project
-git pull
+git fetch && git pull
 python3 measurement_tool.py $ts
 scp -r "project_archive/"$ts".json" compf@37.221.197.246:project/project_archive
