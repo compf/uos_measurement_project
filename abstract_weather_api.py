bb8a5f8ed432bd0a721bb1c16d21dc3126e19c94
fiadleh <fiadleh@uos.de>
2022-05-26 21:03:23 +0200

    add Meteomatics, Aeris and Foreca APIs

--- abstract_weather_api.py
+++ abstract_weather_api.py
@@ -1,6 +1,7 @@
 import http.client
 #from weatherbit.api import Api
 import requests
+from requests.auth import HTTPBasicAuth
 from datetime import datetime
 
 class WeatherInformation:
@@ -187,12 +188,81 @@ class BuienRadar(AbstractWeatherAPI):
         result.wind_speed=best_station["windspeed"]
         result.description=best_station["weatherdescription"] # sadly in dutch
         return result
+
+class Meteomatics(AbstractWeatherAPI):
+    def load_data(self,lat:float,lon:float):
+        # Valid until 2022-06-08
+        api_user = 'vera_dast'
+        api_password = '5RF3ecV1f3'
+        current_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        url = f'https://api.meteomatics.com/{current_date}/t_2m:C,precip_1h:mm,wind_speed_10m:ms,sunrise:sql,sunset:sql,weather_symbol_1h:idx/{lat},{lon}/json'
+        response = requests.get(url, verify=False, auth=HTTPBasicAuth(api_user, api_password))
+        data = response.json()
+        
+        info = WeatherInformation()
+        info.temperature = data['data'][0]['coordinates'][0]['dates'][0]['value']
+        info.rain        = data['data'][1]['coordinates'][0]['dates'][0]['value']
+        info.wind_speed  = data['data'][2]['coordinates'][0]['dates'][0]['value']
+        info.sun_rise  = data['data'][3]['coordinates'][0]['dates'][0]['value']
+        info.sun_set  = data['data'][4]['coordinates'][0]['dates'][0]['value']
+        info.description  = data['data'][5]['coordinates'][0]['dates'][0]['value']
+        
+        return info
+
+class Aeris(AbstractWeatherAPI):
+    def load_data(self,lat:float,lon:float):
+        # Valid until 2022-06-26
+        client_id = 'fNhvFwdyUKxsfowQsQ6rD'
+        client_secret = 'MFmQhE0EugSaG8R41jYo4jhUDgbhU7MOG5bhrAE4'
+
+        url = f'https://api.aerisapi.com/conditions/{lat},{lon}?client_id={client_id}&client_secret={client_secret}'
+        response = requests.get(url)
+        data = response.json()
+        
+        info = WeatherInformation()
+        info.temperature = data['response'][0]['periods'][0]['tempC']
+        info.rain        = data['response'][0]['periods'][0]['precipMM']
+        info.wind_speed  = data['response'][0]['periods'][0]['windSpeedKPH']
+        info.humidity  = data['response'][0]['periods'][0]['humidity']
+        info.last_updated  = data['response'][0]['periods'][0]['timestamp']
+        info.air_pressure  = data['response'][0]['periods'][0]['pressureMB']
+        info.description  = data['response'][0]['periods'][0]['weatherPrimary']        
+        return info
+class Foreca(AbstractWeatherAPI):
+    def load_data(self,lat:float,lon:float):
+        # Only for Laar !!!!!! lat and lon have no effects
+        # Valid until 2022-06-26
+        token_url = 'https://pfa.foreca.com/authorize/token'
+        log_vars = {'user': 'mymob-12345', 'password': 'NoeHKNYSwaZI'}
+        access_token = requests.post(token_url, data=log_vars).json()['access_token']
+        authorization_header = {'Authorization': 'Bearer '+access_token}
+        current_url = 'https://pfa.foreca.com/api/v1/current/102882115' # 102882115 = Laar's Location ID
+        #url2 = 'https://pfa.foreca.com/api/v1/location/search/Laar' # search for location id by name
+        response = requests.post(current_url, headers=authorization_header)
+        data = response.json()
+
+        info = WeatherInformation()
+        info.temperature = data['current']['temperature']
+        info.rain        = data['current']['precipProb']
+        info.wind_speed  = data['current']['windSpeed']
+        info.thunder  = data['current']['thunderProb']
+        info.humidity  = data['current']['relHumidity']
+        info.last_updated  = data['current']['time']
+        info.air_pressure  = data['current']['pressure']
+        info.description  = data['current']['symbol'] #https://developer.foreca.com/resources
+        
+        return info
+
+
 apis_dict={
     "buienradar":BuienRadar,
     "openweatherMap":OpenWeatherMap,
     "accuWeather":AccuWeather,
     "weatherAPI":WeatherAPI,
-    "weatherBit":WeatherBitIO
-
-
+    "weatherBit":WeatherBitIO,
+    "meteomatics":Meteomatics,
+    "aeris":Aeris,
+    "foreca":Foreca
 }
+    
