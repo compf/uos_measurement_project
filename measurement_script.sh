0d128da95ec183652a619492dff78214dd4b7c7f
timo.schoemaker <timo.schoemaker@hotmail.de>
2022-05-27 11:40:40 +0200

    removing upload, not needed

--- measurement_script.sh
+++ measurement_script.sh
@@ -3,4 +3,4 @@ ts=$(date +%s)
 ts=$(($ts-$(($ts%60)))) # resetting to removing effect of possible additional seconds
 #cd /home/pi/project
 python3 measurement_tool.py --time $ts --conf $1
-scp -r "project_archive/"$ts".json" compf@37.221.197.246:project/project_archive
+#scp -r "project_archive/"$ts".json" compf@37.221.197.246:project/project_archive
